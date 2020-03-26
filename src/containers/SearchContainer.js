dc40ce01fce6be278b1f2c60e721ec616b2f083e
github username <yinagit@gmail.com>
2020-03-25 23:52:12 -0400

    added logo, searchContainer and searchReducer for structure

---
+++ src/containers/SearchContainer.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import {connect} from 'react-redux'
+
+class AccountsContainer extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+      Search Flights
+      </div>
+    )
+  }
+}
+
+
+export default AccountsContainer
