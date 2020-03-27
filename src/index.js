2580c7ba3ab504a16ebe580f946cccbd589cab2b
github username <yinagit@gmail.com>
2020-03-26 20:08:04 -0400

    mapDispatchtoprops.error

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import {BrowserRouter as Router} from 'react-router-dom'
-import searchReducer from './reducers/searchReducer'
-
-
+// import {createStore, applyMiddleware, compose} from 'redux'
+// import thunk from 'redux-thunk'
+// import { Provider } from 'react-redux'
+// import {BrowserRouter as Router} from 'react-router-dom'
+// import searchReducer from './reducers/searchReducer'
 import App from './App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
-let myStore = createStore(searchReducer, composeEnhancers(applyMiddleware(thunk)))
+// let myStore = createStore(searchReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider store={myStore}>
-    <Router>
+  // <Provider store={myStore}>
+    // <Router>
       <App />
-    </Router>
-  </Provider>
+    // </Router>
+  // </Provider>
   ,
   document.getElementById('root'))
